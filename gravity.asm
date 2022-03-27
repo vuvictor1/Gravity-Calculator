788436e96989d1d2d329bbd36b9108963121cc9b
Victor V Vu <78053016+vuvictor1@users.noreply.github.com>
2022-03-27 11:33:49 -0700

    Update gravity.asm

--- gravity.asm
+++ gravity.asm
@@ -10,6 +10,15 @@
 ; This File: gravity.asm                                                                    *
 ; Description: Takes user input, verify float(call atof), and calculates gravity.           *
 ;********************************************************************************************
+;                                                                                           *
+; Copyright (C) 2022 Victor V. Vu                                                           *
+; This program is free software: you can redistribute it and/or modify it under the terms   * 
+; of the GNU General Public License version 3 as published by the Free Software Foundation. * 
+; This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY  *
+; without even the implied Warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. * 
+; See the GNU General Public License for more details. A copy of the GNU General Public     *
+; License v3 is available here:  <https://www.gnu.org/licenses/>.                           *                                                                                          
+;********************************************************************************************
 
 extern printf
 extern scanf
